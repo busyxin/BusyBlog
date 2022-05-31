9e763e842f4f1d394de4c543da9bb2f55d821772
busyxin <xin@busyxin.com>
2022-05-31 21:08:49 +0300

    Update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Busyxin",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Personal blog about tech, self-development, love, life and death in the 21st century.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
